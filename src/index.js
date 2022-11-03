6e17b54c7cca8496086d2aced79c94d543f3de23
morozzA <masik.moroz@gmail.com>
2022-11-03 14:20:21 +0200

    add library

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+// import ImageApi from './image-api';
+// import markUp from './markUp';
+
+formEl = document.querySelector('#search-form');
+inputEl = document.querySelector('input[name="searchQuery"]');
+loadMoreBtn = document.querySelector('.load-more');
+console.log(inputEl);
